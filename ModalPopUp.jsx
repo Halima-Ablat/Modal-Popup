9b32378bc8221e0b5b04153f77b5cfca4b49ca5a
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2025-01-16 19:41:48 +1100

    Add files via upload

--- ModalPopUp.jsx
+++ ModalPopUp.jsx
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function ModalPopUp() {
-
   const [displayBox, setDisplayBox] = useState(true);
+
+  const buttonRef = useRef();
 
   function handleModalPopup() {
     document.body.style.backgroundColor = "lightgreen";
-    setDisplayBox(true)
-    
+    setDisplayBox(true);
+    buttonRef.current.style.display = "none";
   }
 
-  function handleDisappearBox(){
-   setDisplayBox(false);
-   document.body.style.backgroundColor = "";
+  function handleDisappearBox() {
+    setDisplayBox(false);
+    document.body.style.backgroundColor = "";
   }
 
   return (
     <>
-      <button onClick={handleModalPopup}>Open Modal Popup</button>
-      {displayBox && ( <div className="container">
-        <div className="box">
-          <span className="close-button" onClick={handleDisappearBox}>❌</span>
-          <header>Customized Header</header>
-          <p>Customized body</p>
-          <footer>Customized Footer</footer>
+      {displayBox === true ? (
+        <div className="container">
+          <div className="box">
+            <span className="close-button" onClick={handleDisappearBox}>
+              ❌
+            </span>
+            <header>Customized Header</header>
+            <p>Customized body</p>
+            <footer>Customized Footer</footer>
+          </div>
         </div>
-      </div>
-    )}
-     
+      ) : (
+        <div className="modal-btn">
+          <button onClick={handleModalPopup} ref={buttonRef}>
+            Open Modal Popup
+          </button>
+        </div>
+      )}
     </>
   );
 }
